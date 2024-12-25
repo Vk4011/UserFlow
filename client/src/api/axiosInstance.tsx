e030aab2296223ce8242551d99d20a572bd89a2f
G VIKRAM <97166893+Vk4011@users.noreply.github.com>
2024-12-25 14:16:26 +0530

    Update axiosInstance.tsx

--- client/src/api/axiosInstance.tsx
+++ client/src/api/axiosInstance.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000', // Replace with your base URL
+  // https://userflowbackend.onrender.com/
+     baseURL: 'https://userflowbackend.onrender.com/', 
   timeout: 10000, // Optional timeout
 });
 
